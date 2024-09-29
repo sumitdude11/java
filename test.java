ff14c64a5ec247584b6a2bcc588fb0ceb3d2a80a
sumit rathore <sumitrathore1108@gmail.com>
2024-09-30 02:29:19 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,20 @@
+
 public class Test {
-	 public static void main(String[] args){
-        System.out.println("Hello world!");
-        
+   
+    public static void main(String[] args) {
+        byte a = 23;
+        String s = "sumit";
+        s= String.valueOf(a);
+        System.out.println(s);
+        Calculator cal = new Calculator();
+        cal.add(a);
     }
 }
+class Calculator{
+   public int a=1;
+    public int add(int ta) {
+        System.out.println("gdshj"+ta);
+        
+        return 0;
+    }
+};
